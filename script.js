f9d7440593fbc8ec3dcc6fa5c2e368a8001cf244
SrdjanLakic <srdjan949@gmail.com>
2022-07-17 14:10:06 -0400

    description style updated

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 window.addEventListener('load', () => {
   let lon;
   let lat;
-  let temperature = document.querySelector('.temperature-degree');
-  let temperatureDescription = document.querySelector(
+  const temperatureDegree = document.querySelector('.temperature-degree');
+  const temperatureDescription = document.querySelector(
     '.temperature-description'
   );
-  let timezoneLocation = document.querySelector('.location-timezone');
+  const timezoneLocation = document.querySelector('.location-timezone');
+  let degreeSection = document.querySelector('.temperature');
+  const temperatureSpan = document.querySelector('.temperature span');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       lon = position.coords.longitude;
       lat = position.coords.latitude;
-      console.log(position);
-      console.log(lat, lon);
 
-      const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=33605dcddc9f4b1eb7bf9f252c99f90e&include=minutely`;
+      const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=33605dcddc9f4b1eb7bf9f252c99f90e`;
       fetch(api)
         .then((data) => {
           return data.json();
         })
         .then((data) => {
           console.log(data);
-          temperature.textContent = data.data[0].temp;
+          const { temp, timezone } = data.data[0];
+          temperatureDegree.textContent = temp;
           temperatureDescription.textContent = data.data[0].weather.description;
-          timezoneLocation.textContent = data.data[0].timezone;
+          timezoneLocation.textContent = timezone;
+
+          let fahrenheit = temp * (9 / 5) + 32;
+
+          degreeSection.addEventListener('click', () => {
+            if (temperatureSpan.textContent === 'C') {
+              temperatureSpan.textContent === 'F';
+              temp === Math.floor(fahrenheit);
+            } else {
+              temperatureSpan.textContent === 'C';
+              temperatureDegree.textContent === temp;
+            }
+          });
         });
     });
   } else {
